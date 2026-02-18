9f4587d4e3a76ea8bee1f3a0e07d43a581d0c9a9
Tanveer <tanveershaikh.3101@gmail.com>
2026-02-18 19:46:49 +0530

    Performed multiple inputs in a single line

---
+++ Day_1/4_Multi_Input.py
@@ -0,0 +1,10 @@
+str1, str2 = input("Enter the strings: ").split()
+
+print(f"String 1: {str1}")
+print(f"String 2: {str2}")
+
+
+num1, num2 = map(int,input("Enter the two numbers: ").split())
+
+print(f"Num1: {num1}")
+print(f"Num2: {num2}")
