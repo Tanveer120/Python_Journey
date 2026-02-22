d4c86963f0e756a0e69b3183423696dcc7b47d20
Tanveer <tanveershaikh.3101@gmail.com>
2026-02-22 20:18:06 +0530

    Operators and String Operations

---
+++ Day_5/string_operators.py
@@ -0,0 +1,9 @@
+str1 = "MSQ"
+str2 = "PPD"
+
+print(f"Concatenation: {str1+str2}")
+print(f"Repetition operator (string * n) str1: {(str1+' ')*5}")
+print(f"str1 length len(string): {len(str1)}")
+print(f"Number to string str(): {str(26)}")
+print(f"Char to Int: {ord('?')}")
+print(f"Int to Char: {chr(26)}")
